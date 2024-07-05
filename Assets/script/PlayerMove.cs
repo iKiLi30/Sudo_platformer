ec1759800b377d30fdd1f88cd7aed00424ca839a
iKiLi30 <jolahmilovski@yahoo.com>
2024-07-06 00:10:01 +0300

    Lesson_9
    added the speed value and the ability to change it in the inspector

--- Assets/script/PlayerMove.cs
+++ Assets/script/PlayerMove.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D _playerRigidbody2D;
-    private float _horizontalStep = 2f;
-    private float _verticalStep = 0f;
+    [SerializeField] private Rigidbody2D _playerRigidbody;
+    [Header("Параметры")]
+    [SerializeField] private float _horizontalStep;
 
     private void Start()
     {   
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        _playerRigidbody.velocity = new Vector2(_horizontalStep, _playerRigidbody.velocity.y);
         
-        _playerRigidbody2D = GetComponent<Rigidbody2D>();
-        _playerRigidbody2D.velocity = new Vector2(_horizontalStep, _verticalStep);
     }
 
 }
