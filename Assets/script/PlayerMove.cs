7db82a40e35e8237918f1d324d5b602caea9ac18
iKiLi30 <jolahmilovski@yahoo.com>
2024-07-06 21:28:41 +0300

    Lesson_10.1
    add _stepRatio

--- Assets/script/PlayerMove.cs
+++ Assets/script/PlayerMove.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMove : MonoBehaviour
-{
+{   private const float _stepRatio = 50f;
     [SerializeField] private Rigidbody2D _playerRigidbody;
     [Header("Параметры")]
-    [SerializeField] private float _horizontalStep = 500;
+    [SerializeField] private float _horizontalStep;
 
     private void Start()
     {   
@@ -15,7 +15,7 @@ public class PlayerMove : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        _playerRigidbody.velocity = new Vector2(_horizontalStep * Time.deltaTime, _playerRigidbody.velocity.y);
+        _playerRigidbody.velocity = new Vector2(_horizontalStep * Time.deltaTime * _stepRatio, _playerRigidbody.velocity.y);
          
     }
 }
